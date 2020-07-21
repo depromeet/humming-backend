cf11307fa7b375e6f47373d83776b09931993c5d
fitz <fitz@hpcnt.com>
2020-07-21 20:45:48 +0900

    [#1] Create project

---
+++ src/main/java/com/depromeet/humming/HummingApplication.java
@@ -0,0 +1,13 @@
+package com.depromeet.humming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HummingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HummingApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/depromeet/humming/example/ExampleController.java
@@ -0,0 +1,33 @@
+package com.depromeet.humming.example;
+
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/examples")
+public class ExampleController {
+
+    @ApiOperation(
+        value = "API 설명",
+        notes = "## 문서 제목 \n" +
+                "--- \n" +
+                "### 문서 작은 제목 \n" +
+                "문서 내용"
+    )
+    @PostMapping
+    public Example example(@RequestBody Example example) {
+        return example;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    private static class Example {
+        private String name;
+        private int age;
+    }
+}
