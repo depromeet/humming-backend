9454be31ccc2741042e82ebdc68088b41e3822eb
JaewonKwon <springkjw@gmail.com>
2020-07-25 16:03:49 +0900

    [#4]Google API 키 빠진거 추가

--- src/main/java/com/depromeet/humming/youtube/Youtube.java
+++ src/main/java/com/depromeet/humming/youtube/Youtube.java
@@ -10,7 +10,8 @@ import java.net.URL;
 public class Youtube {
   public String search(String search) throws IOException {
     String apiUrl = "https://www.googleapis.com/youtube/v3/search";
-    apiUrl += "part=snippet&type=video&maxResults=20&videoEmbeddable=true";
+    apiUrl += "?key=AIzaSyBRa6UEjmyCOrv5ug1jEB8DOs2NuKZFePk";
+    apiUrl += "&part=snippet&type=video&maxResults=20&videoEmbeddable=true";
     apiUrl += "&q=" + URLEncoder.encode(search, "UTF-8");
 
     URL url = new URL(apiUrl);
