c9689e9cec24f58a01a4de1a9c6e43fad150f93f
JaewonKwon <springkjw@gmail.com>
2020-07-25 15:59:02 +0900

    [#4]Youtube Search API 연결

---
+++ src/main/java/com/depromeet/humming/youtube/Youtube.java
@@ -0,0 +1,30 @@
+package com.depromeet.humming.youtube;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URLEncoder;
+import java.net.URL;
+
+public class Youtube {
+  public String search(String search) throws IOException {
+    String apiUrl = "https://www.googleapis.com/youtube/v3/search";
+    apiUrl += "part=snippet&type=video&maxResults=20&videoEmbeddable=true";
+    apiUrl += "&q=" + URLEncoder.encode(search, "UTF-8");
+
+    URL url = new URL(apiUrl);
+    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+    con.setRequestMethod("GET");
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+    String inputLine;
+    StringBuffer response = new StringBuffer();
+    while ((inputLine = br.readLine()) != null) {
+      response.append(inputLine);
+    }
+    br.close();
+
+    return response.toString();
+  }
+}
