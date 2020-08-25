824f951cb893ba595105b831f9d54071c62ec5ca
joongsoo <wndtn853@gmail.com>
2020-08-25 12:26:08 +0900

    [#10] Add cors configuration

--- src/main/java/com/depromeet/humming/config/WebConfig.java
+++ src/main/java/com/depromeet/humming/config/WebConfig.java
@@ -2,9 +2,11 @@ package com.depromeet.humming.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
+@EnableWebFlux
 public class WebConfig implements WebFluxConfigurer {
 
     @Override
