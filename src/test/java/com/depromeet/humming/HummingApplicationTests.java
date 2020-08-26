1bc1c64a1146cbae196ff1af64538e29ff9ac31c
joongsoo <wndtn853@gmail.com>
2020-08-26 22:04:29 +0900

    [#10] Add cors configuration

--- src/test/java/com/depromeet/humming/HummingApplicationTests.java
+++ src/test/java/com/depromeet/humming/HummingApplicationTests.java
@@ -1,9 +1,7 @@
 package com.depromeet.humming;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class HummingApplicationTests {
 
     @Test
